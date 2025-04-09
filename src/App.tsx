b87dd0c86d52ed99f33f7a62c34ca923f916323c
marianaiha <ihamariana@gmail.com>
2025-04-09 13:18:22 -0300

    cookbook 16 aou 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import './App.css'
-import Home from './pages/home/Home'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './pages/cadastro/Cadastro';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Navbar />
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
           </Routes>
         </div>
-      <Footer />
-    </BrowserRouter>
- 
-    
-  )
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
