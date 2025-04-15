ab9cf870c18284611cf0432f2fd2f77fa99a80f8
marianaiha <ihamariana@gmail.com>
2025-04-15 17:09:49 -0300

    perfil e toastAlert

--- src/components/postagens/cardpostagens/CardPostagens.tsx
+++ src/components/postagens/cardpostagens/CardPostagens.tsx
@@ -13,7 +13,7 @@ function CardPostagem({ postagem }: CardPostagensProps) {
             <div>
                 <div className="flex w-full bg-indigo-400 py-2 px-4 items-center gap-4">
                     <img
-                        src={postagem.usuario?.foto}
+                        src={postagem.usuario?.foto || 'https://www.svgrepo.com/show/192244/man-user.svg'}
                         className='h-12 rounded-full'
                         alt={postagem.usuario?.nome} />
                     <h3 className='text-lg font-bold text-center uppercase'>
